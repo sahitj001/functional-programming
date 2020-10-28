5e932bb7fb611c6db1a91cdb76579a4e87c10838
Jordy S <jordy.sahit@live.nl>
2020-10-28 12:36:03 +0100

    trying to clean data

--- rdw.js
+++ rdw.js
@@ -1,13 +1,27 @@
-let endpoint = 'https://opendata.rdw.nl/resource/qidm-7mkf.json';
+const endpoint = 'https://opendata.rdw.nl/resource/qidm-7mkf.json'
 
-getData(endpoint);
+getData(endpoint)
 .then(data => {
-	const result = filterData(data, 'UsageIdDesc');
+	const result = filterData(data, 'usageiddesc')
 	console.log(result);
-} )
+	const cleanedData = cleanData(result)
+	console.log(cleanedData);
+	})
 
 async function getData(url){
-	const response = await fetch(url);
-	const data = await response.json();
-	return data;
+	const response = await fetch(url)
+	const data = await response.json()
+	return data
 }
+
+const filterData = (data, column) => {
+	return data.map(result => result[column])
+}
+
+const cleanData = (data => {
+	console.log('test')
+	if(data = undefined){
+		const none = data.concat('geen')
+		console.log('added geen' ,none)
+	}
+})
