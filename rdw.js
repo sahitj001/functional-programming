ada6b71244950a7941d7ee912e16aabf0807090e
sahitj001 <jordy.sahit@live.nl>
2020-11-02 15:54:33 +0100

    trying to put the area id and capacity in an object. it works but i also need to get data from two other datasets. i have no idea how i can prevent sending too much requests

--- rdw.js
+++ rdw.js
@@ -1,14 +1,42 @@
-const endpoint = 'https://opendata.rdw.nl/resource/qidm-7mkf.json'
+// const endpoint = 'https://opendata.rdw.nl/resource/qidm-7mkf.json?$limit=2053'
+// 'https://cors-anywhere.herokuapp.com/'
+console.log('test')
+// Here I will get the AreaId and the capacity of the parking garages
+const url1 = 'https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=1563'
 
-getData(endpoint)
-.then(data => {
-	const result = filterData(data, 'usageiddesc')
-	console.log(result);
-	const cleanedData = cleanData(result)
-	console.log(cleanedData);
+// Here I will get the UUID out of the API so I later can find the location of the garage
+const url2 = 'https://opendata.rdw.nl/Parkeren/Open-Data-Parkeren-PARKEERGEBIED/mz4f-59fw?$limit=7928'
+
+const url3 =  'https://npropendata.rdw.nl/parkingdata/v2/'
+
+// Getting garage ID so I can look up its unique identifier. I will also get the parkingspots from this API.
+getData('https://cors-anywhere.herokuapp.com/' + url2)
+	.then(data => {
+
+		const id = filterData(data, 'areaid')
+		console.log(id);
+		const parkingSpots = filterData(data, 'capacity')
+		console.log(parkingSpots)
+
+		// Put both values in an object
+		const element = {
+			areaId : id,
+			capacity : parkingSpots
+		}
+		console.log(element.areaId.length)
+
+		// Now I am getting the area ID where you also can find the UUID. The plan is to compare the areaId from the previous dataset with this one.
+		// getData('https://cors-anywhere.herokuapp.com/' + garageId)
+		// 	.then(data => {
+		// 		const matchingId = filterData(data, 'areaid')
+		// 		console.log(matchingId)
+		// 	})
+		// 	return element;
+		// const cleanedData = cleanData(result)
+		// console.log(cleanedData);
 	})
 
-async function getData(url){
+async function getData(url) {
 	const response = await fetch(url)
 	const data = await response.json()
 	return data
@@ -18,10 +46,13 @@ const filterData = (data, column) => {
 	return data.map(result => result[column])
 }
 
-const cleanData = (data => {
-	console.log('test')
-	if(data = 'undefined'){
-		const none = data.concat('geen')
-		console.log('added geen' ,none)
-	}
-})
+// const cleanData = (data => {
+// 	console.log('test')
+// 	for(i = 0; i<data.length; i++ ){
+// 		if(data = 'undefined'){
+// 			const none = data
+// 			console.log('added geen ' ,none)
+// 		}
+// 	}
+
+// })
