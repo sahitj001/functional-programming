800df52015e37d6cd2087b301d66caca3318c693
sahitj001 <jordy.sahit@live.nl>
2020-11-04 14:27:04 +0100

    i accidently requested 8000 times for some data so now i got rate limited, currently working on a local version instead of keeping to fetch

--- rdw.js
+++ rdw.js
@@ -1,6 +1,21 @@
-// const endpoint = 'https://opendata.rdw.nl/resource/qidm-7mkf.json?$limit=2053'
-// 'https://cors-anywhere.herokuapp.com/'
-console.log('d3 loaded', d3)
+
+
+// //playing with data from folder so I won't get rate limited
+const jsonData = require('./v2')
+console.log(jsonData.ParkingFacilities.length)
+console.log(jsonData.ParkingFacilities[0].identifier)
+
+const garageId = []
+
+// for (let index = 0; index < jsonData.ParkingFacilities.length; index++) {
+// 	console.log('test')
+// 	// garageId.push(jsonData.ParkingFacilities.identifier)
+// 	// console.log(garageId)
+// }
+
+// console.log('d3 loaded', d3)
+
+
 // Here I will get the AreaId and the capacity of the parking garages
 const url1 = 'https://opendata.rdw.nl/resource/b3us-f26s.json?$limit=1563'
 
@@ -12,34 +27,23 @@ const url3 = 'https://npropendata.rdw.nl/parkingdata/v2/'
 const cors = 'https://cors-anywhere.herokuapp.com/'
 
 // Getting garage ID so I can look up its unique identifier. I will also get the parkingspots from this API.
-const data1 = getData(cors + url3)
-	.then(data => {
-		// Get unique ID of garage
-		const identifier = filterData(data, 'identifier')
-		console.log(identifier);
-		// const parkingSpots = filterData(data, 'capacity')
-		// console.log(parkingSpots)
+// const data1 = getData(cors + url3)
+// 	.then(data => {
+// 		// Get unique ID of garage
+// 		const identifier = filterData(data, 'identifier')
+// 		console.log(identifier);
 
-		// Put both values in an object
-		// const element = {
-		// 	areaId : id,
-		// 	capacity : parkingSpots
-		// }
-		// console.log(element.areaId.length)
+// 		return identifier
+// 	})
 
-		// Now I am getting the area ID where you also can find the UUID. The plan is to compare the areaId from the previous dataset with this one.
-		// getData('https://cors-anywhere.herokuapp.com/' + garageId)
-		// 	.then(data => {
-		// 		const matchingId = filterData(data, 'areaid')
-		// 		console.log(matchingId)
-		// 	})
-		// 	return element;
-		return data
-	})
 	// .then(information => {
 	// 	console.log('informationnn', information)
-	// 	const capacity = getData(cors + url3 + information)
-	// 	console.log(capacity)
+
+	// 	for (let index = 0; index < information.length; index++) {
+	// 		const element = array[index];
+
+	// 	}
+
 	// 	return information
 	// })
 
